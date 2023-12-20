cf3913c06cd6d1ec3a544deb83b580f88df2a375
jin <a11718trannhuy@gmail.com>
2023-12-20 09:28:29 +0700

    update code

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,97 @@
-import React from 'react'
-import './App.css'
+import React, { useCallback, useRef } from 'react';
+import ReactFlow, {
+    useNodesState,
+    useEdgesState,
+    addEdge,
+    useReactFlow,
+    ReactFlowProvider,
+    MiniMap,
+    Background,
+    Controls
+} from 'reactflow';
 
-function App() {
+import 'reactflow/dist/style.css';
+
+const initialNodes = [
+    {
+        id: '0',
+        type: 'input',
+        data: { label: 'Node' },
+        position: { x: 0, y: 50 },
+    },
+];
+
+let id = 1;
+const getId = () => `${id++}`;
+
+const AddNodeOnEdgeDrop = () => {
+
+    const reactFlowWrapper = useRef(null);
+    const connectingNodeId = useRef(null);
+
+    const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+    const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+
+    const { screenToFlowPosition } = useReactFlow();
+
+    const onConnect = useCallback((params) => {
+        connectingNodeId.current = null;
+        setEdges((eds) => addEdge(params, eds))
+    },[]);
+
+    const onConnectStart = useCallback((_, { nodeId }) => {
+        connectingNodeId.current = nodeId;
+    }, []);
+
+    const onConnectEnd = useCallback((event) => {
+        if (!connectingNodeId.current) return;
+
+        const targetIsPane = event.target.classList.contains('react-flow__pane');
+
+        if (targetIsPane) {
+            // we need to remove the wrapper bounds, in order to get the correct position
+            const id = getId();
+            const newNode = {
+                id,
+                position: screenToFlowPosition({
+                    x: event.clientX,
+                    y: event.clientY,
+                }),
+                data: { label: `Node ${id}` },
+                origin: [0.5, 0.0],
+            };
+
+            setNodes((nds) => nds.concat(newNode));
+            setEdges((eds) =>
+                eds.concat({ id, source: connectingNodeId.current, target: id }),
+            );
+        }
+    },[screenToFlowPosition]);
+
     return (
-        <div className='w-screen h-screen flex items-center justify-center'>123</div>
-    )
-}
+        <div className="w-screen h-screen" ref={reactFlowWrapper}>
+            <ReactFlow
+                nodes={nodes}
+                edges={edges}
+                onNodesChange={onNodesChange}
+                onEdgesChange={onEdgesChange}
+                onConnect={onConnect}
+                onConnectStart={onConnectStart}
+                onConnectEnd={onConnectEnd}
+                fitView
+                fitViewOptions={{ padding: 2 }}
+                nodeOrigin={[0.5, 0]}
+            >
+                <MiniMap />
+                <Controls />
+                <Background />
+            </ReactFlow>
+        </div>
+    );
+};
 
-export default App
+export default () => (
+    <ReactFlowProvider>
+        <AddNodeOnEdgeDrop />
+    </ReactFlowProvider>
+);
