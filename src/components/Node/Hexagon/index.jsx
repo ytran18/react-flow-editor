24d8fc7874db775c890314a9adf04e8d17f97b96
jin <a11718trannhuy@gmail.com>
2023-12-27 12:21:17 +0700

    update hexagon style

--- src/components/Node/Hexagon/index.jsx
+++ src/components/Node/Hexagon/index.jsx
@@ -17,11 +17,18 @@ const Hexagon = ({data, isConnectable}) => {
         setState(prev => ({...prev, styles: styles?.[0]?.style || {}}));
     },[store]);
 
+    const stroke = (state.styles?.borderStyle !== 'none' && state.styles?.borderStyle !== 'hidden') ? state.styles?.borderColor : 'none';
+
+    const strokeDasharray = {
+        'dotted': '2, 1',
+        'dashed': '5 2',
+    }[state.styles?.borderStyle];
+
     return (
         <>
             <svg width="120" height="60" className="relative">
                 <g transform="translate(2, 2)">
-                    <path d="M0,28 L11.600000000000001,0 L104.4,0 L116,28 L104.4,56 L11.600000000000001,56 Z" fill={state.styles?.hexagonBg || '#eee'} strokeWidth="2" strokeDasharray={state.styles?.borderStyle === 'dotted' ? '0.8,1' : '0'} stroke={state.styles?.borderColor} fillOpacity="0.8"></path>
+                    <path d="M0,28 L11.600000000000001,0 L104.4,0 L116,28 L104.4,56 L11.600000000000001,56 Z" fill={state.styles?.hexagonBg || '#eee'} strokeWidth="2" strokeDasharray={strokeDasharray} stroke={stroke} fillOpacity="0.8"></path>
                 </g>
             </svg>
             <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform">{data.label}</div>
