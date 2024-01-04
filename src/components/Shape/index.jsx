a1cf80cc6ca265c4ff493031a0abf48d5fa1bc8e
jin <a11718trannhuy@gmail.com>
2024-01-04 15:28:11 +0700

    implement undo-redo UI

--- src/components/Shape/index.jsx
+++ src/components/Shape/index.jsx
@@ -4,6 +4,9 @@ import { Panel } from 'reactflow';
 
 import { circle, roundedRetangle,retangle, hexagon, arrowRetangle, diamond, cylinder, triangle, parallelogram, plus } from '../../assets/icons';
 
+import IconNext from '../../assets/next.svg';
+import IconBack from '../../assets/back.svg';
+
 const Shape = () => {
 
     const onDragStart = (event, nodeType) => {
@@ -12,45 +15,50 @@ const Shape = () => {
     };
 
     return (
-        <Panel 
-            position="top-left"
-            className="bg-white w-[200px] h-fit rounded-lg shadow-md p-2"
-        >
-            <aside>
-                <div className="text-sm text-center mb-2 select-none">Drag shape to the canvas</div>
-                <div className="w-full grid grid-cols-4">
-                    <div onDragStart={(event) => onDragStart(event, 'circle')} draggable>
-                        {circle}
+        <Panel position="top-left" className="flex">
+            <div 
+                className="bg-white w-[200px] h-fit rounded-lg shadow-md p-2 mr-2"
+            >
+                <aside>
+                    <div className="text-sm text-center mb-2 select-none">Drag shape to the canvas</div>
+                    <div className="w-full grid grid-cols-4">
+                        <div onDragStart={(event) => onDragStart(event, 'circle')} draggable>
+                            {circle}
+                        </div>
+                        <div onDragStart={(event) => onDragStart(event, 'rounded-rectangle')} draggable>
+                            {roundedRetangle}
+                        </div>
+                        <div onDragStart={(event) => onDragStart(event, 'rectangle')} draggable>
+                            {retangle}
+                        </div>
+                        <div onDragStart={(event) => onDragStart(event, 'hexagon')} draggable>
+                            {hexagon}
+                        </div>
+                        <div onDragStart={(event) => onDragStart(event, 'diamond')} draggable>
+                            {diamond}
+                        </div>
+                        <div onDragStart={(event) => onDragStart(event, 'arrow-rectangle')} draggable>
+                            {arrowRetangle}
+                        </div>
+                        <div onDragStart={(event) => onDragStart(event, 'triangle')} draggable>
+                            {triangle}
+                        </div>
+                        <div onDragStart={(event) => onDragStart(event, 'cylinder')} draggable>
+                            {cylinder}
+                        </div>
+                        <div onDragStart={(event) => onDragStart(event, 'parallelogram')} draggable>
+                            {parallelogram}
+                        </div>
+                        <div onDragStart={(event) => onDragStart(event, 'plus')} draggable>
+                            {plus}
+                        </div>
                     </div>
-                    <div onDragStart={(event) => onDragStart(event, 'rounded-rectangle')} draggable>
-                        {roundedRetangle}
-                    </div>
-                    <div onDragStart={(event) => onDragStart(event, 'rectangle')} draggable>
-                        {retangle}
-                    </div>
-                    <div onDragStart={(event) => onDragStart(event, 'hexagon')} draggable>
-                        {hexagon}
-                    </div>
-                    <div onDragStart={(event) => onDragStart(event, 'diamond')} draggable>
-                        {diamond}
-                    </div>
-                    <div onDragStart={(event) => onDragStart(event, 'arrow-rectangle')} draggable>
-                        {arrowRetangle}
-                    </div>
-                    <div onDragStart={(event) => onDragStart(event, 'triangle')} draggable>
-                        {triangle}
-                    </div>
-                    <div onDragStart={(event) => onDragStart(event, 'cylinder')} draggable>
-                        {cylinder}
-                    </div>
-                    <div onDragStart={(event) => onDragStart(event, 'parallelogram')} draggable>
-                        {parallelogram}
-                    </div>
-                    <div onDragStart={(event) => onDragStart(event, 'plus')} draggable>
-                        {plus}
-                    </div>
-                </div>
-            </aside>
+                </aside>
+            </div>
+            <div className="bg-white p-1 h-fit flex items-center rounded-lg shadow-md">
+                <img src={IconBack} className="mx-1 p-1 hover:bg-[rgb(226,232,240)] rounded-md cursor-pointer"/>
+                <img src={IconNext} className="mx-1 p-1 hover:bg-[rgb(226,232,240)] rounded-md cursor-pointer"/>
+            </div>
         </Panel>
     );
 };
